f77fcd4d9c66fee66c58c9d5df9bf84661acd986
zhudi <zhudi@geely.com>
2018-06-07 09:30:18 +0800

    删除没有用的引用

--- module/testautomaticmodel/src/main/java/com/demo/testautomatic/AppDemo.java
+++ module/testautomaticmodel/src/main/java/com/demo/testautomatic/AppDemo.java
@@ -1,26 +1,21 @@
 package com.demo.testautomatic;
 
-import java.io.File;
 import java.net.URL;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class AppDemo {
-//    private static AppiumDriver<AndroidElement> driver;
     private static AndroidDriver<AndroidElement> driver;
 
     @Before
@@ -35,9 +30,7 @@ public class AppDemo {
         //capabilities.setCapability("app", app.getAbsolutePath());
         capabilities.setCapability("appPackage", "cn.zhudi.mvpdemo");//被测app的包名
         capabilities.setCapability("appActivity", ".WelcomeActivity");//被测app的入口Activity名称
-//        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);//把以上配置传到appium服务端并连接手机
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-//        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         System.out.println("App is launched!");
     }
 
@@ -53,7 +46,6 @@ public class AppDemo {
         WebElement el = driver.findElementById("cn.zhudi.mvpdemo:id/ivCar");
         //WebElement el = driver.findElement(By.name("Add Contact"));
         el.click();
-
 //        List<WebElement> textFieldsList = driver.findElementsByClassName("android.widget.EditText");
         //textFieldsList.get(0).sendKeys("Some Name");
         //textFieldsList.get(2).sendKeys("Some@example.com");
